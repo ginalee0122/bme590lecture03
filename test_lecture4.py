51a1cdb3eab02e7c73117baaacf0548cc616cfd7
leohu6 <31520223+leohu6@users.noreply.github.com>
2017-09-07 16:31:15 -0400

    2 test files

---
+++ test_lecture4.py
@@ -0,0 +1,18 @@
+import pytest
+
+def add(a, b):
+    c = a + b
+    if c < 0:
+        return 0
+    else:
+        return c
+
+def test_intAdd():
+    assert add(1,2) == 3
+    assert add(0,0) == 0
+    assert add(-1,-2) == 0
+
+def test_doubleAdd():
+    assert add(.001,1.11) == 1.111
+    assert add(-0.2,0.1) == 0
+    assert add(1.2,1.8) == 3
